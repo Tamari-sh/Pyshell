aecce1f0cb265c0b806533e10a98987578459080
Tamari-sh <tamari.sh2018@leyada.net>
2025-07-01 17:18:14 +0300

    fix ls args

--- pyshell.py
+++ pyshell.py
@@ -3,18 +3,32 @@ import glob
 import shutil
 import sys
 from colorama import Fore, Back, Style
+from typing import List
 
 
-def pyshell_ls() -> None:
+def pyshell_ls(cmd: List[str]) -> None:
     """Function to recreate ls in python"""
     current_path = os.getcwd()
 
-    for root, dirs, files in os.walk(current_path):
-        for directory in dirs:
-            print(Fore.BLUE + directory)
-            dirs.remove(directory)
-        for file in files:
-            print(Fore.GREEN + file)
+    if len(cmd) > 0:
+        # if path of alternative directory passed
+        path = cmd[0]
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
+                for directory in dirs:
+                    print(Fore.BLUE + directory)
+                    dirs.remove(directory)
+                for file in files:
+                    print(Fore.GREEN + file)
+        else:
+            print(f"ls cannot access '{path}': No such directory")
+    else:
+        for root, dirs, files in os.walk(current_path):
+            for directory in dirs:
+                print(Fore.BLUE + directory)
+                dirs.remove(directory)
+            for file in files:
+                print(Fore.GREEN + file)
 
     print(Style.RESET_ALL)
 
@@ -62,16 +76,16 @@ def main() -> None:
         input_cmd = input("💗 ")
         actions = input_cmd.split(" ")
         action = actions[0]
+        actions.remove(action)
 
         if action == "ls":
-            pyshell_ls()
+            pyshell_ls(actions)
         elif action == "cd":
             pyshell_cd(actions[1])
         elif action == "pwd":
             pyshell_pwd()
         elif action == "echo":
             # join the string to print
-            actions.remove(action)
             echo = " ".join(actions)
             pyshell_echo(echo)
         elif action == "quit":
